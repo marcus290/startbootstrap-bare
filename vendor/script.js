0300756e8b974282579e4f4de8c49f2732fb095b
marcus290 <36244342+marcus290@users.noreply.github.com>
2018-05-20 07:04:13 +0800

    20/05/2018 implemented center pane

--- vendor/script.js
+++ vendor/script.js
@@ -1,6 +1,7 @@
     function changeMenu(name) {
         closePChat();
         endVid();
+        closeDocPane();
         if (name === "Person0") {closePMenu(); return;}
 
         var allButtons = document.querySelectorAll(".menuButton");
@@ -38,6 +39,8 @@
         var headSelector = "div.heads#" + name + " span";
         document.querySelector("#privChat h5").innerHTML = document.querySelector(headSelector).innerHTML;
         document.getElementById("privChat").classList.remove("invisible");
+        var alertSelector = "div#" + name + " span.chatAlert";
+        document.querySelector(alertSelector).style.opacity = "0";
     }
     function closePChat() {
         document.getElementById("privChat").classList.add("invisible");
@@ -48,7 +51,68 @@
     function endVid() {
         document.getElementById("vidChat").classList.add("invisible");
     }
+    function openDocPane(name) {
+        document.getElementById("docPane").classList.remove("invisible");
+        var alertSelector = "div#" + name + " span.docAlert";
+        document.querySelector(alertSelector).style.animation = "none";
+        document.querySelector(alertSelector).style.opacity = "0";
+    }
+    function closeDocPane() {
+        document.getElementById("docPane").classList.add("invisible");
+    }
     function greyOut(buttonId) {
         document.getElementById(buttonId).classList.add("greyed-out");
+    }
+
+    /* SOURCE https://www.w3schools.com/w3css/w3css_slideshow.asp */
+    var slideIndex = 3;
+    
+    function plusDivs(n) {
+        showDivs(slideIndex += n);
+    }
+    
+    function showDivs(n) {
+        var i;
+        var x = document.getElementsByClassName("mySlides");
+        if (n > x.length) {slideIndex = 1} 
+        if (n < 1) {slideIndex = x.length} ;
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+        x[slideIndex-1].style.display = "block";
+        document.querySelector("p.img-title").innerHTML = x[slideIndex-1].title;
+        console.log("showDivs is applying index: " + n);
+    }
+    /* END SOURCE */
+
+    /* image handling in problem space */
+    function enlargeImage(num) {
+        document.querySelector("div.problem-set-img-enlarged").style.display = "table-cell";
+        document.querySelector("div.problem-tiled-container").style.display = "none";
+        showDivs(num);
+        console.log(num);
+    }
+    function closeImgEnlarge() {
+        document.querySelector("div.problem-set-img-enlarged").style.display = "none";
+        document.querySelector("div.problem-tiled-container").style.display = "table-cell";
+    }
+
+    function displayModal() {
+        document.getElementById('myModal').style.display = "block";
+    }
+    function closeEdit() {
+        document.getElementById('myModal').style.display = "none";
+    }
+    function displayConnie() {
+        document.getElementById('connie-modal').style.display = "block";
+    }
+    function closeConnie() {
+        document.getElementById('connie-modal').style.display = "none";
+    }
+    function displaySam() {
+        document.getElementById('sam-modal').style.display = "block";
+    }
+    function closeSam() {
+        document.getElementById('sam-modal').style.display = "none";
     }
 
